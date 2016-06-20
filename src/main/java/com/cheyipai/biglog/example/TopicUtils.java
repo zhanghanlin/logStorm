b13f99ddee31fca82158d7468646a97711e8cdbc
zhanghanlin <andyzhanghanlin@163.com>
2016-06-20 14:45:01 +0800

    update code

--- src/main/java/com/cheyipai/biglog/example/TopicUtils.java
+++ src/main/java/com/cheyipai/biglog/example/TopicUtils.java
@@ -2,6 +2,11 @@ package com.cheyipai.biglog.example;
 
 import kafka.admin.TopicCommand;
 
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import static com.cheyipai.biglog.utils.Prop.zkHosts;
 
 public class TopicUtils {
@@ -45,13 +50,34 @@ public class TopicUtils {
         command(options);
     }
 
+    public static void delete(String topic) {
+        String[] options = new String[]{
+                "--delete",
+                "--zookeeper",
+                zkHosts,
+                "--topic",
+                topic,
+        };
+        command(options);
+    }
+
     public static void command(String[] options) {
         TopicCommand.main(options);
     }
 
     public static void main(String[] args) {
-        if (args != null) {
-            create(args[0]);
+        if (args != null && args.length > 0) {
+            try {
+                PropertyDescriptor descriptor = new PropertyDescriptor(args[0], TopicUtils.class);
+                Method method = descriptor.getReadMethod();
+                method.invoke(args[1]);
+            } catch (IntrospectionException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
